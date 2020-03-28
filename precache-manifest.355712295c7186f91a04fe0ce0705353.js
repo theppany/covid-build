64004879663fc01601e79c07de31a410b82cf3b8
theppany <theppany@gmail.com>
2020-03-28 13:48:16 +0700

    first commit

---
+++ precache-manifest.355712295c7186f91a04fe0ce0705353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346186bc3e351cfd002cc9e649e9c484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d446cb93029ee9342b9a",
+    "url": "/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "e22c0144f35d3b7a0748",
+    "url": "/static/css/main.86d003ea.chunk.css"
+  },
+  {
+    "revision": "d446cb93029ee9342b9a",
+    "url": "/static/js/2.b3b2ba70.chunk.js"
+  },
+  {
+    "revision": "4d7511add9a103636723d818b190f2ba",
+    "url": "/static/js/2.b3b2ba70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22c0144f35d3b7a0748",
+    "url": "/static/js/main.e8c4010b.chunk.js"
+  },
+  {
+    "revision": "0598f9686dcddb157cc4",
+    "url": "/static/js/runtime-main.eed1f24c.js"
+  }
+]);
